1477fb2e9a8373925cfce9f468d9c81442f076e7
Darshit Bhuva <darshitbhuva9330@gmail.com>
2024-11-26 11:19:46 +0530

    Onboarding form functionality completed

---
+++ lib/mongoose.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+
+    mongoose.set('strictQuery', true);
+
+    if(!process.env.MONGODB_URL)
+        return console.log("MongoDb Url not found");
+
+    if(isConnected)
+        return console.log("Already connected to MongoDB");
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+
+        console.log("Connected to MongoDB");
+        
+    } catch (error) {
+        
+    }
+        
+        
+}
